214b4eba9824eaec27a4418fcb5c053d31332a45
Andy <andybaird@gmail.com>
2021-02-24 22:57:40 -0700

    Mapping value types

--- src/Game.ts
+++ src/Game.ts
@@ -1,9 +1,19 @@
 
 
-const cardsJson: CardJsonValues[] = require('./cardvalues.json');
+const cardsJson: CardJsonValues[] = require('./cards.json');
+const noblesJson: NobleJsonValues[] = require('./nobles.json');
+
+interface NobleJsonValues {
+  points: number;
+  diamond: number | null;
+  sapphire: number | null;
+  emerald: number | null;
+  ruby: number | null;
+  onyx: number | null;
+}
 
 interface CardJsonValues {
-  gem: string;
+  gem: 'emerald' | 'sapphire' | 'diamond' | 'ruby' | 'onyx';
   tier: number;
   points: number | null;
   diamond: number | null;
@@ -12,14 +22,6 @@ interface CardJsonValues {
   ruby: number | null;
   onyx: number | null;
 }
-
-interface GemCost {
-  diamond: number;
-  sapphire: number;
-  emerald: number;
-  ruby: number;
-  onyx: number;
-} 
 
 export enum Gem {
   Ruby, Sapphire, Onyx, Emerald, Diamond
@@ -33,9 +35,9 @@ class Card {
   points: number;
   gem: Gem;
   tier: Tier;
-  costs: GemCost;
+  costs: GemStash;
 
-  constructor(points: number, gem: Gem, tier: Tier, costs: GemCost) {
+  constructor(points: number, gem: Gem, tier: Tier, costs: GemStash) {
     this.points = points;
     this.gem = gem;
     this.tier = tier;
@@ -55,15 +57,15 @@ class CardPile {
 
 export interface Noble {
   points: number;
-  costs: GemCost[];
+  costs: GemStash;
 }
 
 export interface GemStash {
-  rubies: number;
-  sapphires: number;
-  diamonds: number;
-  onyxes: number;
-  emeralds: number;
+  [Gem.Ruby]: number;
+  [Gem.Sapphire]: number;
+  [Gem.Diamond]: number;
+  [Gem.Onyx]: number;
+  [Gem.Emerald]: number;
 }
 
 const stringToGemMap = {
@@ -74,14 +76,20 @@ const stringToGemMap = {
   'sapphire': Gem.Sapphire
 };
 
-const numberToTierMap = [ undefined, Tier.I, Tier.II, Tier.III ];
+const numberToTierMap = [ Tier.I, Tier.II, Tier.III ];
 
 const mapCardValuesJsonToCardType = (cardValues: CardJsonValues[]) => cardValues.map(
   c => new Card(
-    c.points,
-    stringToGemMap(c.gem),
-    numberToTierMap[c.tier],
-
+    c.points || 0,
+    stringToGemMap[c.gem],
+    numberToTierMap[c.tier-1],
+    {
+      [Gem.Ruby]: c.ruby || 0,
+      [Gem.Sapphire]: c.sapphire || 0,
+      [Gem.Diamond]: c.diamond || 0,
+      [Gem.Onyx]: c.onyx || 0,
+      [Gem.Emerald]: c.emerald || 0
+    }
   )
 )
 
@@ -104,28 +112,16 @@ export class Game  {
     this.tierIIICards = new CardPile(Tier.III, []);
 
     this.tierIDrawPile = new CardPile(Tier.I, cards.filter(c => c.tier = Tier.I));
-    this.tierIIDrawPile = new CardPile(Tier.II, []);
-    this.tierIIIDrawPile = new CardPile(Tier.III, []);
+    this.tierIIDrawPile = new CardPile(Tier.II, cards.filter(c => c.tier = Tier.II));
+    this.tierIIIDrawPile = new CardPile(Tier.III, cards.filter(c => c.tier = Tier.III));
 
-  }
-}
+    this.gems = {
+      [Gem.Ruby]: 6,
+      [Gem.Sapphire]: 6,
+      [Gem.Diamond]: 6,
+      [Gem.Onyx]: 6,
+      [Gem.Emerald]: 6
+    }
 
-interface IPlayer {
-  name: string;
-  cards: Card[];
-  nobles: Noble[];
-  gems: GemStash;
-}
-
-export class Player implements IPlayer {
-  name: string = '';
-  cards: Card[] = [];
-  nobles: Noble[] = [];
-  gems: GemStash = {
-    rubies: 0,
-    sapphires: 0,
-    diamonds: 0,
-    onyxes: 0,
-    emeralds: 0
   }
 }
